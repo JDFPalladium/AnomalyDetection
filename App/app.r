a05ed3174667d3f371c5e1351e1e544f0625ef45
kevsakaguchi <94194155+kevsakaguchi@users.noreply.github.com>
2022-08-09 16:45:52 -0400

    Introjs script complete!

--- App/app.r
+++ App/app.r
@@ -13,15 +13,55 @@ library(shinyWidgets)
 library(rintrojs)
 
 ui <- dashboardPage(
-  dashboardHeader(title = "Anomaly Detection"),
+  dashboardHeader(title = "Anomaly Detection",
+                  tags$li(class = "dropdown", id= "download",
+                          dropMenu(
+                            dropdownButton("Info",  icon = icon('download')),
+                            conditionalPanel(condition = "input.type == 'Recommender'",
+                            actionButton("download_rec", "Click to download Recommender outputs.")
+                            ),
+                            conditionalPanel(condition = "input.type == 'Time Series'",
+                            actionButton("download_rec", "Click to download Times Series outputs.")
+                            ),
+                            placement = "bottom",
+                            arrow = TRUE
+                            )
+                          
+                  ),
+                  # dropdownMenu(
+                  #   type = "notifications", 
+                  #   headerText = strong("Download"), 
+                  #   icon = icon("download"), 
+                  #   badgeStatus = NULL,
+                  #   notificationItem(
+                  #     actionButton("download", "Click to download all selected"),
+                  #     icon = icon("download")
+                  #   )),
+                  tags$li(
+                    a(
+                      strong("INFO"),
+                      height = 40,
+                      href = "https://github.com/JDFPalladium/AnomalyDetection/blob/main/README.md",
+                      title = "",
+                      target = "_blank",
+                      ),
+                    class = "dropdown"
+                  )
+                    ),
+  ####Recommender Sidebar ####
   dashboardSidebar(
     introjsUI(),
     sidebarMenu(
-      fluidRow(
+      fluidRow(column(12, offset =1,
         conditionalPanel(
           condition = "input.type == 'Recommender'",
-      actionButton("help", "Press for Recommender instructions"))),
-      
+          tags$br(),
+      actionButton("help", HTML("Press for Recommender <br/> instructions"))))),
+      fluidRow(column(12, offset =1,
+                      conditionalPanel(
+                        condition = "input.type == 'Time Series'",
+                        tags$br(),
+                        actionButton("help2", HTML("Press for Time Series <br/> instructions"))))),
       fluidRow(column(12,
                       div(id="step4",
           selectInput("type", "Type",
@@ -30,8 +70,7 @@ ui <- dashboardPage(
       #### Recommender Menu ####
       
         conditionalPanel(condition = "input.type == 'Recommender'",
-          fluidRow(column(12,
-                      div(id="step2",
+          fluidRow(column(12,div(id="step2",
         menuItem(tabName = "recommender", startExpanded = TRUE,
                  tags$br(),
                  fluidRow(column(12,
@@ -48,16 +87,22 @@ ui <- dashboardPage(
                                       ".csv",
                                       ".xlsx"))
                  )))
-        ))),
-                 fluidRow(column(10, offset =1,
+        )))),
+                 fluidRow(column(8, offset =2,
                                  div(id="step6",
                  actionButton("recdata", "Run Data Check"),
                                  ))),
-                fluidRow(column(10,offset =1,
-                                div(id="step7",    
-                 tags$b("Switch ON if you want to run,"), tags$br(),
-                 tags$b("OFF if you do not"),tags$br(),tags$br(),
-                 
+                  tags$br(),
+        #Step 7
+                
+                 fluidRow(column(12, offset=1,
+                                 tags$b("Switch ON if you want to run,"), tags$br(),
+                                  tags$b("OFF if you do not"),tags$br(),tags$br()
+                 )),
+        
+        menuItem(tabName="disaggregations", id="step7",startExpanded = TRUE,
+                 tags$br(),
+                 tags$h5("Observation Disaggregations"),
                  "Select the analyses to run with MER", tags$br(),
                  "data disaggregated by sex and age", tags$br(), 
                  
@@ -77,9 +122,12 @@ ui <- dashboardPage(
                           "obs. of the same age group",
                           startExpanded = TRUE,
                           switchInput(inputId = "age", value = FALSE))
-                                ))),
-                  fluidRow(column(10, offset=1,
-                                  div(id="step8", 
+                 ),
+                 tags$br(),
+                   
+        menuItem(tabName="disaggregations",startExpanded = TRUE, id="step8",
+                 tags$br(),
+                 tags$h5("Facility Disaggregations"),
                  "Select the analyses to run with MER", tags$br(),
                  "data aggregated at the facility", tags$br(),
                  
@@ -93,32 +141,33 @@ ui <- dashboardPage(
                           tags$br(),
                           "obs. of the same PSNU",
                           switchInput(inputId = "psnu", value = FALSE))
-                                  ))),
-        fluidRow(column(10, offset=1,
-                        div(id="step9",
-                 "Switch ON to discount indicators", tags$br(),
-                 "with very low values.",tags$br(), 
-                 menuItem("Minimum Threshold", tabName = "min_thresh",startExpanded = TRUE,
-                          numericInput(inputId = "min_thresh", label = "", value = 10))
-                        ))),
-        fluidRow(column(10, offset=1,
-                        div(id="step10",
-                 
-                 "Switch ON if you want outputs to ", tags$br(),
-                 "include both anomalous and non-anomalous", tags$br(),
-                 "non-anomalous obs.",
-                 tags$br(),
-                 menuItem("Return", tabName = "return",startExpanded = TRUE,
-                          
-                          switchInput(inputId = "return", value = FALSE))
-                        )))
         )
-      ),
+                 ,
+        menuItem(tabName = "settings", startExpanded = TRUE, collapsible=FALSE,
+                 tags$br(),
+                 tags$h5("Detection Settings"),
+                 menuItem("Minimum Threshold", tabName = "min_thresh", id = "step9",startExpanded = TRUE,
+                          "Switch ON to discount indicators", tags$br(),
+                          "with very low values.",tags$br(), 
+                          numericInput(inputId = "min_thresh", label = "", value = 10)),
+                 tags$br(),
+                 menuItem("Return", tabName = "return", id = "step10", startExpanded = TRUE, offset=2,
+                          "Switch ON if you want outputs to ", tags$br(),
+                          "include both anomalous and", tags$br(),
+                          "non-anomalous obs.",         
+                          switchInput(inputId = "return", value = FALSE))
+                        )
+        )
+        
+    
+      ,
       #### Time Series Menu ####
       conditionalPanel(
         condition = "input.type == 'Time Series'",
         menuItem(tabName = "time", startExpanded = TRUE,
                  tags$br(),
+                 fluidRow(column(12,
+                                 div(id="tsstep4",
                  tags$b("Data Upload"),
                  numericInput("year", label = "Data Fiscal Year", value = 2022),
                  selectInput("quarter", "Quarter",
@@ -128,18 +177,24 @@ ui <- dashboardPage(
                            multiple = TRUE,
                            accept = c("text/csv",
                                       "text/comma-separated-values,text/plain",
-                                      ".csv")),
-                 numericInput("datasets", "Number of datasets:", 10, min =1, max = 10),
-                 "Switch ON to discount indicators", tags$br(),
-                 "with very low values.",tags$br(), 
-                 menuItem("Minimum Threshold", tabName = "min_thresh",startExpanded = TRUE,
-                          numericInput(inputId = "min_thresh", label = "", value = 10)),
-                 
-                 "Switch ON if you want outputs to ", tags$br(),
-                 "include both anomalous and non-anomalous", tags$br(),
-                 "non-anomalous obs.",
-                 menuItem("Return", tabName = "return",startExpanded = TRUE,
-                          
+                                      ".csv"))
+                 )))
+                 ),
+                 fluidRow(column(12,
+                                 div(id="tsstep5",
+                 numericInput("datasets", "Number of datasets:", 10, min =1, max = 10)
+                 ))),
+          menuItem(tabName = "settings", startExpanded = TRUE,  collapsible=FALSE,
+                  tags$br(),
+                  tags$h5("Detection Settings"),
+                 menuItem("Minimum Threshold", tabName = "min_thresh", id="tsmintresh", startExpanded = TRUE,
+                          "Switch ON to discount indicators", tags$br(),
+                          "with very low values.",tags$br(), 
+                                                    numericInput(inputId = "min_thresh", label = "", value = 10)),
+                 menuItem("Return", tabName = "return", id="tsreturn", startExpanded = TRUE,
+                          "Switch ON if you want outputs to ", tags$br(),
+                          "include both anomalous and non-anomalous", tags$br(),
+                          "non-anomalous obs.",
                           switchInput(inputId = "return", value = TRUE))
         )
       )
@@ -147,17 +202,23 @@ ui <- dashboardPage(
     )
   ),
   dashboardBody(
-    h2(textOutput('title')),
-    conditionalPanel(
+    fluidRow(div(id="body_title",
+                 (h2(textOutput('title')))
+                 )),
+    fluidRow(div(id="rec_datachecks",
+      conditionalPanel(id="rec_datachecks",
       condition = "output.rec_data",
       box(title = "Data Checks", width = 12,
           "Summary Table or Score Card")
-    ),
-    conditionalPanel(
+    )
+    )),
+    fluidRow(div(id="rec_summarytable",
+      conditionalPanel(
       condition = "output.rec_sum",
-      box(title = "Summary Table:..", width = 12,
+      box(title = "Summary Table:..", width = 12, 
           "Summary Table or Score Card")
-    ),
+    )
+    )),
     conditionalPanel(
       condition = "output.rec1",
       box(title = "Observations: Each obs. compared against all obs.", width = 12,
@@ -183,33 +244,40 @@ ui <- dashboardPage(
       box(title = "PSNU: Each obs. compared against all obs. of the same PSNU", width = 12,
           "PSNU Disaggregate Table")
     ),
-    conditionalPanel(
+    fluidRow(div(id="ts_summary",
+      conditionalPanel(
       condition = "output.ts1",
       box(title = "Times Series: ", width = 12,
           "...")
     )
+    ))
     
     
   )
 )
 
 server <- function(input, output, session) {
+  #### Recommender IntroJS 
   steps <- reactive(
     data.frame(
-      element=c(".main-header", "#step2", ".sidebar-toggle", "#step4", "#step2", "#step6", "#step7", "#step8", "#step9", "#step10"),
+      element=c(".main-header", ".sidebar-toggle", "#step4", "#step5", "#step6", "#step7", "#step8", "#step9", "#step10", "#body_title", "#rec_datachecks", "#rec_summarytable", "#center", "#download"),
       intro=c(
-        "The original Anomaly Detection tools were created as a series of R studio scripts. This ShinyApp converts the same functions of the scripts into an online application.",
-        "This is the main interactive sidebar menu. This is where you will upload data, define disaggregations and other detection settings.",
-        "This is a button that allows to close and open the sidebar.",
+        HTML("The original Anomaly Detection tools were created as a series of R studio scripts. This ShinyApp converts the same functions of the scripts into an online application.<br/><br/> <b>These are the Recommender Tool instructions.</b> <br/> Click next to continue with instructions or click skip and change the 'Type' for Time Series instructions."),
+        "This is a button that allows to close and open the sidebar. <br/>The sidebar menu is where you will upload data, define disaggregations and other detection settings.",
         "Here you can choose which type of analysis you want to run either Recommender or Times Series.<br/><br/> The sidebar menu options will change based on this selection",
         "Here you will upload your data for a Recommender analysis. <br/> 1. Enter the four-digit fiscal year. <br/> 2. Choose the correct quarter for the data. <br/> 3. Locate the file on your computer. <br/> <br/> NOTE: Only .csv, .xlsx, or .txt MER data files work function properly.",
         "Click the Run Data Check button after uploading your data. This will double check the file to make sure the named variables are included. <br/><br/> A notice will appear in the Data Checks box to confirm data consistency.",
         "Here are the three primary disggregation options: Observation, Sex, and Age",
         "Here are the two additional disggregation options: Facility, and Age",
         "After the disaggregations are selected, there are two more settings to define. <br/> First, you can decide to discount indicators with very low values. This is automaticaly set to ten but can be altered depending on the context",
-        "Second, you can chose whether or not to display both anomalous and non-anomalous outputs in the final tables. <br/> For large data sets, we would highly recommend to leave this setting off."
+        "Second, you can chose whether or not to display both anomalous and non-anomalous outputs in the final tables. <br/> For large data sets, we would highly recommend to leave this setting off.",
+        "Here are all of the output will appear. Each box will contain a different output.",
+        "This is the box for Data Checks. After uploading and running the data check, the content here will tell you in the appropriate variables are included in the uploaded dataset.",
+        "This is the Summary Table which will provide a broad overview of the detection. This box will always appear.",
+        "All other outputs for each of the disaggregations will appear ONLY when the switch is turned ON. The the rest of the outputs and their display boxes will not populate until the corresponding swtiches are turned on.",
+        "After you have defined all desired disaggregations and settings, click here to download all outputs. <br/> <br/> NOTE: Before clicking, make sure all output boxes display the corresponding data table!"
         ),
-      position=c("right", "right", "bottom", "right", "right", "right", "right", "right", "right", "right")
+      position=c("right", "bottom", "right", "right", "right", "right", "right", "right", "right", "top", "bottom","bottom", "bottom", "left")
     )
   )
   observeEvent(input$help,
@@ -218,10 +286,48 @@ server <- function(input, output, session) {
                                       "nextLabel"="Next",
                                       "prevLabel"="Previous",
                                       "skipLabel"="Skip"
-                       ),
-                       events = list("oncomplete"=I('alert("Done")'))
+                       )
+                       # ,
+                       # events = list("oncomplete"=I('alert("Done")'))
                )
   )
+  
+  #### Time Series IntroJS
+  steps2 <- reactive(
+    data.frame(
+      element=c(".main-header", ".sidebar-menu", ".sidebar-toggle", "#tsstep4", "#tsstep5", "#tsmintresh", "#tsreturn", "#ts_summary", "#download"),
+      intro=c(
+        "The original Anomaly Detection tools were created as a series of R studio scripts. This ShinyApp converts the same functions of the scripts into an online application.<br/><br/> <b>These are the Time Series Tool instructions.</b> <br/> <br/> Click next to continue with instructions or click skip and change the 'Type' for Recommender instructions.",
+        "This is the main interactive sidebar menu. This is where you will upload data sets and define your detection settings.",
+        "This button allows you to close and open the sidebar.",
+        "Here you will upload your data for a Time Series analysis. <br/> 1. Enter the four-digit fiscal year. <br/> 2. Choose the correct quarter for the data. <br/> 3. Locate the files on your computer. Make sure all files are in the same folder on your computer. <br/> <br/> NOTE: Only .csv, .xlsx, or .txt MER data files work function properly. For ease of use, make sure all files are the same format.",
+        "Enter the number of datasets/files you uploaded in the previous step. The default number is 10 and the minimum number requried is 2.",
+        "After the the data upload, there are two settings to define. <br/> First, you can decide to discount indicators with very low values. This is automaticaly set to ten but can be altered depending on the context",
+        "Second, you can chose whether or not to display both anomalous and non-anomalous outputs in the final tables. <br/> For large data sets, we would highly recommend to leave this setting off.",
+        "Second, you can chose whether or not to display both anomalous and non-anomalous outputs in the final tables. <br/> For large data sets, we would highly recommend to leave this setting off.",
+        "Click here to download all Time Series outputs. <br/> <br/> NOTE: Before clicking, make sure all output boxes display the corresponding data table!"
+        ),
+      position=c("right", "right", "bottom", "bottom", "right", "right", "right", "bottom", "left")
+    )
+  )
+  observeEvent(input$help2,
+               introjs(session,
+                       options = list(steps=steps2(),
+                                      "nextLabel"="Next",
+                                      "prevLabel"="Previous",
+                                      "skipLabel"="Skip"
+                       )
+                       )
+  )
+  
+  output$title <- renderText({
+    if(input$type=="Recommender"){
+      paste("Recommender Anomaly Detetction")
+    }
+    else if(input$type=="Time Series"){
+      paste("Time Series Anomaly Detection")
+    }
+  })
   
   output$title <- renderText({
     if(input$type=="Recommender"){
