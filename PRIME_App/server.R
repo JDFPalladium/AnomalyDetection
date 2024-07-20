b71e0f55fe544640743c0e5738dec74372dec81f
Fausto Lopez <flopez@baosystems.com>
2024-07-20 15:16:11 +0000

    - try data frame change

--- PRIME_App/server.R
+++ PRIME_App/server.R
@@ -786,6 +786,7 @@ server <- function(input, output, session) {
         })
         if(exists("all_outputs")){
           z <- all_outputs %>% mutate(outlier_sp = ifelse(outlier_sp == 1, "Yes", "No"))
+          z <- as.data.frame(z)
           reactable::reactable(z)
           
           # output$rec1 = DT::renderDT(
